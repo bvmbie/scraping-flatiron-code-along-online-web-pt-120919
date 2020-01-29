fe7404980d88582dded8e77e292a5f8a5b780402
Henna Vahatiitto <hnnamv@gmail.com>
2020-01-29 19:01:14 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -10,7 +10,6 @@ class Scraper
     # responsible for using Nokogiri and open-uri to grab the entire HTML document from the web page
     
     doc = Nokogiri::HTML(open("http://learn-co-curriculum.github.io/site-for-scraping/courses"))
-    binding.pry
     
     doc.css(".post").each do |post|
       course = Course.new
